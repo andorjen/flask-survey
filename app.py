3a8fd67b9d19a07fbb9408de8f494f74ba7c38e9
Jenny Liu <jennyliuye@gmail.com>
2021-09-09 11:56:51 -0700

    set up home route and start button

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import StrFormatStyle
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
@@ -7,3 +8,16 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get("/")
+def show_survey_page():
+    survey_title = survey.title
+    survey_instructions = survey.instructions
+    return render_template("survey_start.html",title=survey_title,instructions=survey_instructions )
+
+@app.post("/begin")
+
+def start_survey():
+    return redirect("/questions/0")
