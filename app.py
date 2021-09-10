932b2adcc5d2cee29e0d4b7dee32534118b55ab7
Jenny Liu <jennyliuye@gmail.com>
2021-09-09 18:30:50 -0700

    refactor doc strings

--- app.py
+++ app.py
@@ -10,23 +10,28 @@ debug = DebugToolbarExtension(app)
 
 RESPONSE_KEY = "responses"
 
+
 @app.get("/")
 def show_survey_page():
     """Shows the survey start page with title and instructions"""
     session[RESPONSE_KEY] = []
 
     return render_template("survey_start.html",
-        survey = survey )
+                           survey=survey)
+
 
 @app.post("/begin")
 def start_survey():
     """Redirects to the first question when the user clicks Start"""
-    session["current_question_number"] = 0 #refactor to use survey response length
     return redirect("/questions/0")
+
 
 @app.get("/questions/<int:question_number>")
 def display_question(question_number):
-    """Displays question"""
+    """Displays question page based on the question_number in url,
+       if tring to access any question page out of order,redirect to correct question page.
+       if already finished survey, redirect to thank you page.
+    """
 
     if len(session[RESPONSE_KEY]) != question_number:
         question_number = len(session[RESPONSE_KEY])
@@ -38,26 +43,29 @@ def display_question(question_number):
 
     question = survey.questions[question_number].question
     choices = survey.questions[question_number].choices
-    return render_template("question.html", 
-        question = question, 
-        choices = choices) 
+    return render_template("question.html",
+                           question=question,
+                           choices=choices)
+
 
 @app.post("/answer")
 def record_answer_and_redirect():
-    """take the answer from the submitted form,
-    push it into session[RESPONSE_KEY]
-    and then redirect to the next question page
+    """default behavior: take the answer from the submitted form,
+    push it into session[RESPONSE_KEY], and then redirect to the next question page.
+    if already finished survey, redirect to thank you page.
     """
     answer = request.form["answer"]
     responses = session[RESPONSE_KEY]
     responses.append(answer)
     session[RESPONSE_KEY] = responses
-    current_question_number = session["current_question_number"] + 1
-    session["current_question_number"] = current_question_number
+
+    current_question_number = len(session[RESPONSE_KEY])
+
     if current_question_number > len(survey.questions)-1:
         return redirect("/thanks")
     else:
         return redirect(f"/questions/{current_question_number}")
+
 
 @app.get("/thanks")
 def display_thank_you():
